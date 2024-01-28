1ee79ca1c1b17236317eb29c5ba7048c59a17823
sayidAlikm <saycode01@gmail.com>
2024-01-28 11:28:58 +0200

    Added Navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import Link from "next/link";
+import styles from "./navbar.module.css";
 import React from "react";
 import Links from "./links/Links";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="">Sayidi</div>
+    <div className={styles.container}>
+      <Link href={"/"} className={styles.logo}>
+        Sayidi
+      </Link>
       <div className="">
         <Links />
       </div>
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,32 +1,63 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+import Image from "next/image";
+import styles from "./links.module.css";
+import NavLink from "./navLink/NavLink";
+import { useState } from "react";
 
+const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 const Links = () => {
-  const links = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState();
+
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div>
-      {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <Image
+        className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
