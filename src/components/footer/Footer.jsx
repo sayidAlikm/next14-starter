9674411686ed38b6377bfdfe44b3d1afe4d15644
sayidAlikm <saycode01@gmail.com>
2024-01-29 12:03:21 +0200

    Added Contact page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>SayCode</div>
+      <div className={styles.logo}>Sayidi</div>
       <div className={styles.text}>
         Sayidi creative thoughts agency © All rights reserved.
       </div>
